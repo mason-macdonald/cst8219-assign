b3e634f2d8f30ba9f011c274f2b2bd20bd818a66
Malros <Malrosmax@gmail.com>
2022-04-10 14:54:45 -0400

    removed cout

--- Frame.cpp
+++ Frame.cpp
@@ -4,15 +4,12 @@ Frame::Frame(char* name, double time) {
     duration = time;
     frameName = new char[21];
     frameName = strcpy(frameName, name);
-    std::cout << "Frame Name=" << name << "; duration=" << time << "; ADRESS: " << &frameName <<  " ;Does anyone know why this runs twice\n";
-
 }
 
 Frame::Frame(const Frame& src) : Frame(src.frameName, src.duration) {
 }
 
 Frame::~Frame() {
-    std::cout << "Frame Name=" << this->frameName << "; duration=" << this->duration << "; ADRESS: " << &this->frameName << "\n";
     delete frameName;
 }
 
